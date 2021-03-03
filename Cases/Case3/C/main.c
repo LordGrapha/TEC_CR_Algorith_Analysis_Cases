79ef9a90dd34dddfa20f97c6c2bf00390931b33d
Luis Diego Mora Aguilar <39139078+LordGrapha@users.noreply.github.com>
2021-03-03 17:33:03 -0600

    Print - display

--- Cases/Case3/C/main.c
+++ Cases/Case3/C/main.c
@@ -1,7 +1,25 @@
 #include <stdio.h>
 
+
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
+
+   //fill the list
+
+   
+   printf("Reference machine with following specifications:\n\n");
+   printf("    -Device Name: LAPTOP-1OL20O36\n");
+   printf("    -Processor: Intel(R) Core(TM) i5-9300H CPU @ 2.40GHz   2.40 GHz\n");
+   printf("    -Installed RAM: 8.00 GB (7.84 GB usable)\n");
+   printf("    -Graphic Card: Nvidia GeForce GTX 1650\n\n");
+   printf("Running N problem, counting time for 1000000 elements");
+   
+   //get InitialTime here
+
+   //Secuencial Search for the last of the numbers (Worst Case O(n) with n = list size)
+
+   //Get FinalTime here
+
+   //TotalTime = FinalTime - InitialTime
+
    return 0;
 }
